e6635c195b548fe57c53d08865a33a7946e4179a
Samikshadahe <86226271+Samikshadahe@users.noreply.github.com>
2021-06-22 12:52:46 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -3,7 +3,6 @@ package exam;
 public class ShiftElementsInArray {
 
   public void shift(int[] arr) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
     for(int i=0;i<arr.length;i++){
       arr[i+1]=arr[i];
     }
